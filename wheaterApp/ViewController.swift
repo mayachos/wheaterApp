a7b4cee028d27cd7418095e443d78a700ca4b2e0
Mayaka Kobayashi <amacrk@icloud.com>
2022-11-20 01:51:24 +0900

    お天気アプリ

--- wheaterApp/ViewController.swift
+++ wheaterApp/ViewController.swift
@@ -6,14 +6,41 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var tenkiLabel: UILabel!
+    var tenki: String = ""
+    let city = "Kochi"
+    var descriptionWeather: String?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
 
+        yoho()
+    }
+    
+    func yoho() {
+        let text = "https://api.openweathermap.org/data/2.5/weather?q=\(city)&appid=912cb0543da45ffe0a97beb07e894531"
+        let url = text.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
+        AF.request(url!, method: .get, parameters: nil, encoding: JSONEncoding
+                    .default).response { (response) in
+                        switch response.result {
+                        case .success:
+                            let json = JSON(response.data as Any)
+                            self.descriptionWeather = json["weather"][0]["main"].string!
+                            //self.descriptionWeather = json["results"]["album"]["data"].string!
+                            
+                            self.tenkiLabel.text = self.descriptionWeather?.description
+                            
+                        case .failure(let error):
+                            print(error)
+                        }
+                    }
+                        
+    }
 
 }
 
